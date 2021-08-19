4290967cdc8cc2c3b27e4a4d6e923ebfbcfb2741
Simaar <simaar101@gmail.com>
2021-08-19 11:36:39 -0400

    changed folder name and settings

--- UrlShortener/Startup.cs
+++ UrlShortener/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace UrlShortner
+namespace UrlShortener
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace UrlShortner
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UrlShortner", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UrlShortener", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace UrlShortner
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UrlShortner v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UrlShortener v1"));
             }
 
             app.UseHttpsRedirection();
